7e337201df3d942fef57b8e05f4ce0f191424880
maa123 <maa123.lq@gmail.com>
2016-01-08 18:03:30 +0900

    Create memo.js

---
+++ src/memo.js
@@ -0,0 +1,12 @@
+/*
+function printf(a){
+clientMessage(a);
+}
+で少し作りやすくなる...?
+処理が始まる前に
+var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+を書いておくと楽...かも?
+
+
+*/
+//これはメモです(ライセンス適用しないであげてー)
